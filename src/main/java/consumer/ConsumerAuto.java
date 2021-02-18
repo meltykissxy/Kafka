66e632760e55a9f516c0120fb76e0233e29f0640
MeltyKiss <beautifully@188.com>
2021-02-18 08:57:08 +0800

    commit offset sync and async

--- src/main/java/consumer/ConsumerAuto.java
+++ src/main/java/consumer/ConsumerAuto.java
@@ -1,12 +1,14 @@
 package consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import utils.KafkaUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
 /**
  * 自动提交位移
  */
